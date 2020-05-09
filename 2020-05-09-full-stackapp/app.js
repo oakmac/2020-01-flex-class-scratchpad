cdf182f831fbe7f136659c36d1a1abda9904689a
Chris Oakman <chris@oakmac.com>
2020-05-09 12:44:01 -0500

    add db module; show lists on homepage

--- 2020-05-09-full-stackapp/app.js
+++ 2020-05-09-full-stackapp/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const db = require('./lib/db')
 
 const app = express()
 
@@ -41,7 +42,13 @@ const dummyItems = [
 
 // the homepage shows your lists
 app.get('/', function (req, res) {
-  res.render('index', { lists: dummyLists })
+  db.getLists()
+    .then((lists) => {
+      res.render('index', { lists: lists })
+    })
+    .catch(() => {
+      // TODO: show an error page here
+    })
 })
 
 // the list page shows the items in the list
@@ -49,6 +56,19 @@ app.get('/list/:listUUID', function (req, res) {
   res.render('list_page', { listName: 'Dummy List', items: dummyItems })
 })
 
-app.listen(port, () => {
-  console.log('express is listening on port ' + port)
-})
+const startExpressApp = () => {
+  app.listen(port, () => {
+    console.log('express is listening on port ' + port)
+  })
+}
+
+const bootupSequenceFailed = (err) => {
+  console.error('Unable to connect to the database:', err)
+  console.error('Goodbye!')
+  process.exit(1)
+}
+
+// global kickoff point
+db.connect()
+  .then(startExpressApp)
+  .catch(bootupSequenceFailed)
---
+++ 2020-05-09-full-stackapp/lib/db.js
@@ -0,0 +1,41 @@
+const knexLib = require('knex')
+const dbCfg = require('../knexfile')
+
+// this will hold our database connection
+let conn = null
+
+// this should return a promise
+function connect () {
+  return new Promise(function (resolve, reject) {
+    conn = knexLib(dbCfg.development)
+    conn.raw(`SELECT 1 + 1 AS test`)
+      .then((result) => {
+        if (result.rows.length === 1 && result.rows[0].test === 2) {
+          console.log('Database connection established 👍')
+          resolve()
+        } else {
+          reject('Database was unable to do 1 + 1 🤷')
+        }
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
+const getListsQuery = `SELECT * FROM list`
+
+function getLists () {
+  return conn.raw(getListsQuery)
+    .then((result) => {
+      return result.rows
+    })
+}
+
+// -----------------------------------------------------------------------------
+// Public API
+
+module.exports = {
+  connect: connect,
+  getLists: getLists
+}
